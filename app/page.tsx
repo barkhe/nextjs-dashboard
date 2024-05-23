432fa32541f762bba6b99c886c83761685148420
barkhe <wntap3065@gmail.com>
2024-05-23 18:01:17 +0900

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,8 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Acme에 오신 것을 환영합니다.</strong> This is the example
+            for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
